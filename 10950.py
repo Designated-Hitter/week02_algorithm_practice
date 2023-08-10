9f697eb6cacfef03699d94562c35d8fe21c727f4
Designated-Hitter <dosadola@gmail.com>
2023-08-10 21:38:28 +0900

    반복문 초급 문제

--- 10950.py
+++ 10950.py
@@ -1,6 +1,7 @@
-test_case_number = int(input())
-for i in range(test_case_number):
-    test_case_A, test_case_B = map(int(input())).split()
-    print(test_case_A + test_case_B)
+T = int(input())
+for i in range (1, T + 1):
+    A, B = map(int,input().split())
+    print(A + B)
 
-
+#한 줄에 띄어쓰기로 구분 된 변수 둘을 나누어서 정수타입으로 선언하는 법
+#A, B = map(int,input().split())
